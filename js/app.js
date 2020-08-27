6f77b05f1822ec7c549a86506af8e10f5d2b1589
Sebastian Glaser <hakt0r@users.noreply.github.com>
2020-08-27 15:06:18 +0200

    Refactor by hakt0r

--- js/app.js
+++ js/app.js
@@ -1,71 +1,67 @@
+
 const gameFinishedMsg = document.getElementsByClassName("game-finished-msg");
 const gameInstruction = document.getElementsByClassName("game-instruction");
-const gameIntro = document.getElementById("game-intro");
-const startBtn = document.getElementById("start-btn");
-const gameContainer = document.getElementById("container");
-const extra = document.getElementById("extra");
-let bgm = document.getElementById("bgm");
+const gameIntro       = document.getElementById("game-intro");
+const startBtn        = document.getElementById("start-btn");
+const gameContainer   = document.getElementById("container");
+const extra           = document.getElementById("extra");
 
-//----------------------paddle 1-------------------------------------
+const bgm             = document.getElementById("bgm");
+const pop             = document.getElementById("pop");
+const ding            = document.getElementById("ding");
+const win             = document.getElementById("yeah");
 
-const paddle = document.querySelector("#paddle1");
+//------------------scores------------------------------------------
 
-let position = container.offsetHeight / 2;
-let direction = 0;
+const Player1 = document.querySelector("#playerA");
+const Player2 = document.querySelector("#playerB");
+
+let scoreA = 0, scoreB = 0;
+
+//---------------ball  movement---------------------------------
+
+const       ball = container.querySelector("#ball");
+const ballRadius = 10; // TODO: ball.offsetHeight / 2;
+
+let x = 500, y = 300, movementX = 5, movementY = 5;
 
 //------------------paddles-----------------------------------------
 
-const paddle2 = document.querySelector("#paddle2");
+const  paddle1 = document.querySelector("#paddle1");
+let  position1 = container.offsetHeight / 2;
+let direction1 = 0; // -1 up : 0 neutral : 1 down
 
-let position2 = container.offsetHeight / 2;
-let direction2 = 0;
+const  paddle2 = document.querySelector("#paddle2");
+let  position2 = container.offsetHeight / 2;
+let direction2 = 0; // -1 up : 0 neutral : 1 down
 
-document.onkeydown = (event) => {
-  // console.log('keydown', event.code);
-  switch (event.code) {
-    case "KeyW":
-      direction = -1;
-      break;
-    case "KeyS":
-      direction = 1;
-      break;
+const     paddleGlobal = [paddle1, paddle2];
+const halfPaddleHeight = 70; // TODO: paddle1.offsetHeight / 2;
 
-    case "ArrowUp":
-      direction2 = -1;
-      break;
-    case "ArrowDown":
-      direction2 = 1;
-      break;
-  }
-};
+//---------------start game-------------------------------------
 
-document.onkeyup = (event) => {
-  //console.log("keyup", event.code);
-  switch (event.code) {
-    case "ArrowUp":
-      if (direction2 == -1) direction2 = 0;
-      break;
+startBtn.addEventListener("click", startGame);
 
-    case "ArrowDown":
-      if (direction2 == 1) direction2 = 0;
-      break;
-    case "KeyW":
-      if (direction == -1) direction = 0;
-      break;
-    case "KeyS":
-      if (direction == 1) direction = 0;
-      break;
-  }
-};
+function startGame() {
+  bgm.play();
+  bgm.volume = 0.3;
+  gameIntro.style.display = "none";
+  gameContainer.style.display = "block";
+  extra.style.display = "block";
+  setInterval(move, 1000 / 60);
+}
 
-const simulate2 = (time) => {
+//---------------simulation-------------------------------------
+
+function move() {
+  // constrain paddles on y axis
   const upBoundary = 10;
   const downBoundary = container.offsetHeight - 150;
 
-  if (direction !== 0) {
-    position += direction * 10;
-    position = Math.max(upBoundary, Math.min(downBoundary, position));
-    paddle1.style.top = position + "px";
+  if (direction1 !== 0) {
+    position1 += direction1 * 10;
+    position1 = Math.max(upBoundary, Math.min(downBoundary, position1));
+    paddle1.style.top = position1 + "px";
   }
 
   if (direction2 !== 0) {
@@ -73,80 +69,18 @@ const simulate2 = (time) => {
     position2 = Math.max(upBoundary, Math.min(downBoundary, position2));
     paddle2.style.top = position2 + "px";
   }
-};
 
-setInterval(simulate2, 1000 / 60);
-
-//--------------SCORES-----------------------------------------------------
-
-const Player1 = document.querySelector("#playerA");
-const Player2 = document.querySelector("#playerB");
-
-let ding = document.getElementById("ding");
-
-let scoreA = 0;
-let scoreB = 0;
-
-let playerAscore = document.getElementById("paddle1");
-function scoreForB() {
-  if (scoreA < 5) {
-    scoreA++;
-
-    //------sound effect with paddles and boundaries-----------------
-    ding.play();
-    Player1.innerText = scoreA;
-  } else {
-  }
-  resetBall();
-}
-
-let playerBscore = document.getElementById("paddle2");
-function scoreForA() {
-  if (scoreB < 5) {
-    scoreB++;
-
-    //------sound effect with paddles and boundaries-----------------
-    ding.play();
-    Player2.innerText = scoreB;
-  } else {
-  }
-  resetBall();
-}
-
-//---------------ball  movement---------------------------------
-
-const ball = container.querySelector("#ball");
-
-let paddleGlobal = [paddle, paddle2];
-
-let pop = document.getElementById("pop");
-
-let x = 500;
-let y = 300;
-
-let movementY = 5;
-let movementX = 5;
-
-// --------collision-------------------------------------
-
-function ballCollidesWithPaddle(left, top, width, height) {
-  return (
-    x > left - 20 &&
-    x < left + width && // right
-    y > top - 20 &&
-    y < top + height // bottom
-  );
-}
-
-function move() {
-  if (y >= container.offsetHeight - 20 || y <= 0) {
+  // reflect ball on upper and lower wall
+  if ( y >= container.offsetHeight - 20 || y <= 0 ) {
     movementY *= -1;
   }
 
-  if (x >= container.offsetWidth) scoreForB();
-  if (x <= 0) scoreForA();
+  // detect goals / scores
+  if ( x >= container.offsetWidth ) scoreForB();
+  if ( x <= 0                     ) scoreForA();
 
-  paddleGlobal.forEach((p) => {
+  paddleGlobal.forEach( (p) => {
+    // check for collison of ball with each paddle
     const collides = ballCollidesWithPaddle(
       p.offsetLeft,
       p.offsetTop,
@@ -155,19 +89,17 @@ function move() {
     );
 
     if (collides) {
-      //------sound effect with paddles and boundaries-----------------
+      pop.play();         // sound effect
+      movementX *= 1.05;  // accelerate ball by 5%
 
-      pop.play();
-      movementX *= 1.05;
+      // calculate refelction on the y-axis
+      const ballCenterY      = ballRadius + y;
+      const paddleCenterY    = halfPaddleHeight + p.offsetTop;
+      const distanceY        = Math.abs( ballCenterY - paddleCenterY );
+      const reflectionFactor = -2 * ( distanceY / halfPaddleHeight ) + 1;
+      movementY             += reflectionFactor;
 
-      let ballCenterY = 10 + y;
-      let paddleCenterY = 70 + p.offsetTop;
-      let distanceY = Math.abs(ballCenterY - paddleCenterY);
-      const reflectionFactor = -2 * (distanceY / 70) + 1;
-
-      movementY += reflectionFactor;
-
-      if (p === paddle) {
+      if (p === paddle1) {
         movementX = Math.abs(movementX);
       } else {
         movementX = Math.abs(movementX) * -1;
@@ -180,11 +112,37 @@ function move() {
   ball.style.top = `${y}px`;
   ball.style.left = `${x}px`;
 }
-//setInterval(move, 1000 / 60);
+
+function ballCollidesWithPaddle(left, top, width, height) {
+  return (
+    x > left - 20 &&
+    x < left + width && // right
+    y > top - 20 &&
+    y < top + height // bottom
+  );
+}
+
+//--------------SCORES-----------------------------------------------------
+
+function scoreForB() {
+  if (scoreA < 5) {
+    scoreA++;
+    ding.play();  // sound effect with paddles and boundaries
+    Player1.innerText = scoreA;
+  }
+  resetBall();
+}
+
+function scoreForA() {
+  if (scoreB < 5) {
+    scoreB++;
+    ding.play(); // sound effect with paddles and boundaries
+    Player2.innerText = scoreB;
+  }
+  resetBall();
+}
 
 //-------------------------reset function----------
-
-let win = document.getElementById("yeah");
 
 function resetBall() {
   x = container.offsetWidth / 2;
@@ -209,15 +167,42 @@ function resetBall() {
   }
 }
 
-//----------------------------------------------
+//---------------event handlers---------------------------------
 
-startBtn.addEventListener("click", startGame);
+document.onkeydown = (event) => {
+  switch (event.code) {
+    // pladdle1
+    case "KeyW":
+      direction1 = -1;
+      break;
+    case "KeyS":
+      direction1 = 1;
+      break;
+    // pladdle2
+    case "ArrowUp":
+      direction2 = -1;
+      break;
+    case "ArrowDown":
+      direction2 = 1;
+      break;
+  }
+};
 
-function startGame() {
-  bgm.play();
-  bgm.volume = 0.3;
-  gameIntro.style.display = "none";
-  gameContainer.style.display = "block";
-  extra.style.display = "block";
-  setInterval(move, 1000 / 60);
-}
+document.onkeyup = (event) => {
+  switch (event.code) {
+    // pladdle1
+    case "ArrowUp":
+      if (direction2 == -1) direction2 = 0;
+      break;
+    case "ArrowDown":
+      if (direction2 == 1) direction2 = 0;
+      break;
+    // pladdle2
+    case "KeyW":
+      if (direction1 == -1) direction1 = 0;
+      break;
+    case "KeyS":
+      if (direction1 == 1) direction1 = 0;
+      break;
+  }
+};
